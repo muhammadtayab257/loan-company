4e73e0c854309556f92d840bbdd277808e5d7663
Tayyab Siraj <muhammadtayab3022@gmail.com>
2023-06-10 16:40:24 +0500

    minor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 AOS.init();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var rangeInput = document.getElementById('customRange1');
+    var rangeValue = document.getElementById('rangeValue');
+  
+    rangeInput.addEventListener('input', function() {
+      var value = rangeInput.value;
+      rangeValue.textContent = '£' + value;
+    });
+  });
+  
